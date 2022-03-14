ccbb917482f2336bedccae5b6b06d7bc1d06b85f
@109598044 <t109598044@ntut.edu.tw>
2022-03-15 01:01:42 +0800

    20220315 print optim

--- train.py
+++ train.py
@@ -47,6 +47,7 @@ def train(params):
                           {'params': model.semantic_decoder.parameters(), 'weight_decay': 0},
                           {'params': model.depth_decoder.parameters(), 'weight_decay': 0}],
                          lr=params.learning_rate, momentum=params.momentum)
+    print('optimizer:') + type(optimizer).__name__)
 
     # Scheduler
     if params.linear_learning_rate:

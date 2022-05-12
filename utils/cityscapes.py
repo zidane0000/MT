6e0bf406ef6981f90547df499704b52769d04146
zidane0000 <zidane0917@gmail.com>
2022-05-12 23:34:50 +0800

    20220512 fix cityscapes get_traget_suffix

--- utils/cityscapes.py
+++ utils/cityscapes.py
@@ -152,8 +152,8 @@ class Cityscapes(Dataset):
         if target_type == 'instance':
             return '{}_instanceIds.png'.format(mode)
         elif target_type == 'semantic':
-            return '{}_labelIds'.format(mode) # labelIds -> [0. - 18. 255.] / labelTrainIds -> [0. 1. 2. 3. 4. 5. 255.]
-            # return '{}_labelIds.png'.format(mode)
+            # return '{}_labelTrainIds.png'.format(mode) # labelTrainIds -> [0. 1. 2. 3. 4. 5. 255.]
+            return '{}_labelIds.png'.format(mode) # labelIds -> [0. - 18. 255.]
         elif target_type == 'color':
             return '{}_color.png'.format(mode)
         elif target_type == 'disparity':

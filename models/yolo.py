b5094bd86aa06c3d7483b8cb8b5b2d19daafba6a
t109598044 <t109598044@ntut.org.tw>
2022-06-13 14:02:45 +0000

    20220613 fix yolo when inference will diff device

--- models/yolo.py
+++ models/yolo.py
@@ -441,19 +441,13 @@ class IDetect(nn.Module):
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
             if not self.training:  # inference
-                self.grid[i] = self.grid[i].to(x[i].device)
-
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
                 y = x[i].sigmoid()
-                try:
-                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-                except: # sometime happen unequal device
-                    self.grid[i] = self.grid[i].to(y.device)
-                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                y[..., 0:2] = (y[..., 0:2].cuda() * 2. - 0.5 + self.grid[i].cuda())
+                y[..., 0:2] = y[..., 0:2] * self.stride[i]  # xy
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1), x)
